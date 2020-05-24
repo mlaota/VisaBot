0970cdaec51dbf831e994b11c082d45533a2db23
mLaota <39041283+mLaota@users.noreply.github.com>
2020-05-24 15:07:48 -0400

    added discord.py conventional keep_alive

--- visabot/__main__.py
+++ visabot/__main__.py
@@ -1,9 +1,11 @@
 import discord
-from visabot.visabot import VisaBot
 import os
+from visabot.visabot import VisaBot
+from visabot.keep_alive import keep_alive
 
 password = os.getenv('VISABOT_SECRET')
 client = VisaBot(command_prefix='!', sponsor_role='OG Saudi', visa_role='Tourist Visa',
                  announcement_channel='sandscript')
 print('Using discord.py version {}'.format(discord.__version__))
+keep_alive()
 client.run(password)
