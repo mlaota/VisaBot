68f7ba5b94056fa22be46e0cefecce955153abfc
mLaota <39041283+mLaota@users.noreply.github.com>
2020-05-25 19:46:07 -0400

    Closes #4. Reimplemented visa tracking to accomodate several sponsor/tourist pairs with more comprehensive error checking and meaningful help messages
    Also introduces the idea of closing visas through the action "closevisa", but does not implement it yet.

--- visabot/__main__.py
+++ visabot/__main__.py
@@ -4,8 +4,7 @@ from visabot.visabot import VisaBot
 from visabot.keep_alive import keep_alive
 
 password = os.getenv('VISABOT_SECRET')
-client = VisaBot(command_prefix='!', sponsor_role='OG Saudi', visa_role='Tourist Visa',
-                 announcement_channel='visa-status')
+client = VisaBot(command_prefix='!')
 print('Using discord.py version {}'.format(discord.__version__))
 keep_alive()
 client.run(password)
