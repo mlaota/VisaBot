3c303f5f665cbf1e8571572dad8b687fa3e91069
Mohammed Laota <39041283+mlaota@users.noreply.github.com>
2020-12-10 16:14:25 -0500

    update and add keep_alive

--- visabot/__main__.py
+++ visabot/__main__.py
@@ -6,4 +6,5 @@ if __name__ == '__main__':
     password = os.getenv('VISABOT_SECRET')
     client = VisaBot(command_prefix='!')
     print('Using discord.py version {}'.format(discord.__version__))
+    keep_alive()
     client.run(password)
